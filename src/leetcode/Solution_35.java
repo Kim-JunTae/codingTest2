86b9cbddc58d963fd081a6f5097aaa3b6fcdafd6
Jun <61082572+Kim-JunTae@users.noreply.github.com>
2021-07-18 15:44:49 +0900

    feat(solve problem): 34, 46번 풀이
    34. Search Insert Position : 완료
    46. Permutations : 다시 시도 필요

--- src/leetcode/Solution_35.java
+++ src/leetcode/Solution_35.java
@@ -9,13 +9,78 @@ public class Solution_35 {
 
     //문제 35. Search Insert Position
     public static void main(String[] args) {
+        //Example1
+        int[] nums1 = {1, 3, 5, 6};
+        int target1 = 5;
 
+        //Example2
+        int[] nums2 = {1, 3, 5, 6};
+        int target2 = 2;
+
+        //Example3
+        int[] nums3 = {1, 3, 5, 6};
+        int target3 = 7;
+
+        //Example3
+        int[] nums4 = {1, 3, 5, 6};
+        int target4 = 0;
+
+        //Example4
+        int[] nums5 = {1};
+        int target5 = 0;
+
+        int result1 = searchInsert(nums1, target1);
+        int result2 = searchInsert(nums2, target2);
+        int result3 = searchInsert(nums3, target3);
+        int result4 = searchInsert(nums4, target4);
+        int result5 = searchInsert(nums5, target5);
+
+        System.out.println(result1);
+        System.out.println(result2);
+        System.out.println(result3);
+        System.out.println(result4);
+        System.out.println(result5);
     }
-
 
     public static int searchInsert(int[] nums, int target) {
         int result = 0;
 
+        int min = 0;
+        int max = nums.length-1;
+
+        int mid;
+
+        for (int i=0; i<nums.length; i++){
+            mid = (min+max)/2;
+
+            if(nums[mid] == target) return mid;
+
+            if(nums[mid] < target){
+                min = mid+1;
+            }else{
+                max = mid-1;
+            }
+
+            if(min>max) return min;
+        }
+
+
         return result;
     }
+
+//    public static int searchInsert(int[] nums, int target) {
+//        int result = 0;
+//
+//        for(int i=0; i<nums.length; i++){
+//
+//            if(nums[i] >= target){
+//                return i;
+//            }
+//
+//        }
+//
+//        if(nums[nums.length-1] < target) return nums.length;
+//
+//        return result;
+//    }
 }
--- src/leetcode/Solution_46.java
+++ src/leetcode/Solution_46.java
@@ -6,13 +6,77 @@ public class Solution_46 {
 
     //문제 46. Permutations
     public static void main(String[] args) {
+        //Example1
+        int[] nums1 = {1,2,3};
 
+        //Example2
+        int[] nums2 = {0,1};
+
+        //Example3
+        int[] nums3 = {1};
+
+        List<List<Integer>> result1 = permute(nums1);
+        List<List<Integer>> result2 = permute(nums2);
+        List<List<Integer>> result3 = permute(nums3);
+
+        System.out.println("result1 : " + result1);
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println("result2 : " + result2);
+
+        System.out.println("------------------------------------------------");
+
+        System.out.println("result3 : " + result3);
+
+        System.out.println("------------------------------------------------");
     }
 
+    //https://leetcode.com/problems/permutations/discuss/1339709/Java-solution-backtracking-to-return-permutations
 
     public static List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
 
+        int targetIndex = 1;
+
+        //1. 1차 세팅
+        for(int i=0; i<nums.length; i++){
+            List<Integer> item = new ArrayList<>();
+            item.add(nums[i]);
+            result.add(item);
+        }
+
+        makePermutation(targetIndex, nums, result);
+
         return result;
+    }
+
+    public static List<List<Integer>> makePermutation(int targetIndex, int[] nums, List<List<Integer>> result){
+
+        if(targetIndex == nums.length){
+            return result;
+        }
+
+        for(List<Integer> item : result) {
+            for (int j = 0; j < nums.length; j++) {
+                int num = nums[j];
+                boolean chk = false;
+
+                for (int i = 0; i < item.size(); i++) {
+                    if (num != item.get(i)){
+                        chk = true;
+                    }
+                }
+                if (chk) {
+                    item.add(num);
+                    result.add(item);
+                    break;
+                }
+            }
+        }
+
+        targetIndex++;
+
+        return makePermutation(targetIndex, nums, result);
     }
 }
